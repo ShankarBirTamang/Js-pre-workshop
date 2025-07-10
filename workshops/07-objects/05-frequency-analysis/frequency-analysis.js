7e617c9aba4d43163a4aed7341c2355f6c2ab17a
Sankar Bir Tamang <85660085+ShankarBirTamang@users.noreply.github.com>
2025-07-10 16:55:20 +0545

    Update frequency-analysis.js

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,18 +1,11 @@
-// YOUR CODE BELOW
 function frequencyAnalysis(originalString) {
   originalString = originalString.toLowerCase();
   let frequency = {};
-  for (let i = 0; i < originalString.length; i++) {
-    let char = originalString[i];
-    let count = 0;
-    for (let j = 0; j < originalString.length; j++) {
-      if (char === originalString[j]) {
-        count++;
-        frequency[char] = count;
-      }
-    }
+
+  for (let char of originalString) {
+    frequency[char] = (frequency[char] || 0) + 1;
   }
-  console.log(frequency);
+  
   return frequency;
 }
 
